b7a44917f20b074b3cff3582dd9a089e186fcabf
Daria Trubnikova <valkrelex@gmail.com>
2023-01-23 14:08:09 +0300

    feat: add towel sort decision

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+  let reverse = [];
+  if(Array.isArray(matrix)) {
+    for(let i = 0; i < matrix.length; i++) {
+      if(i % 2 !== 0) {
+        reverse = matrix[i].reverse();
+      }
+      if(Array.isArray(matrix[i])) {
+          for(let j = 0; j < matrix[i].length; j++) {
+            if(i % 2 === 0) {
+              res.push(matrix[i][j]);
+            } else {
+              res.push(reverse[j]);
+            }
+        }
+      }
+    }
+  }
+  return res;
 }
